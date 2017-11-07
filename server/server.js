fbfaf8742b48d9aca75dd9ecec9f3af44cc35755
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2017-11-07 11:23:51 -0800

    Implement working chat

--- server/server.js
+++ server/server.js
@@ -2,16 +2,28 @@ const io = require('socket.io')();
 require('dotenv').config()
 const port = process.env.PORT || 8000;
 
+//Initial state that is shared with the client
+let state = {
+  messages: [],
+}
 
+//Starting a server side connection with socket.io
 io.on('connection', (client) => {
-  // here you can start emitting events to the client
-  client.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval: ', interval)
+  //When client callssubscribeSendMessage push the message
+  //into the message array
+  client.on('subscribeSendMessage', (message) => {
+    state.messages.push(message);
+  })
+
+  //When Client calls subscribeToMessages update the client to include
+  //all the messages from the server
+  client.on('subscribeToMessages', (interval) => {
     setInterval(() => {
-      client.emit('timer', new Date());
+      client.emit('move', state.messages);
     }, interval);
   })
 });
 
+//Lisen to the port that is defined in the dotenv file or default of 8000
 io.listen(port);
 console.log('listening on port ', port);
